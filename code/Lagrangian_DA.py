ba9ad4507c0094b4cd088e473218ed51b7c9312a
Zhongrui Wang <55739645+zhongruiw@users.noreply.github.com>
2024-09-23 16:22:36 -0500

    add LSM DA surface flow obs

--- code/Lagrangian_DA.py
+++ code/Lagrangian_DA.py
@@ -1,5 +1,7 @@
 import numpy as np
-# from numba import jit
+from numba import jit
+from numba.typed import Dict
+from numba.core import types
 from scipy import sparse
 from mode_truc import truncate, inv_truncate
 
@@ -31,6 +33,49 @@ def get_A_OU(x,y,K,r1,r2,KX,KY):
     return A
 
 
+def get_A_OU_flow(psi1_k_t, A0_, a0_, F1, F2):
+    A0 = A0_ * psi1_k_t + F1
+    a0 = a0_ * psi1_k_t + F2
+    
+    return A0, a0
+
+
+@jit(nopython=True)
+def get_A_CG_nonlinear(K, psi1_hat, h_hat, linear_A1, linear_a1, k_index_map, list_dic_k_map, kd):
+    # nonlinear summation part for A0, a0, A1 and a1
+    nonlinear_sum_A0 = np.zeros_like(psi1_hat, dtype=np.complex128)
+    nonlinear_sum_a0 = np.zeros_like(psi1_hat, dtype=np.complex128)
+    nonlinear_sum_A1 = np.zeros_like(linear_A1, dtype=np.complex128)
+    nonlinear_sum_a1 = np.zeros_like(linear_a1, dtype=np.complex128)
+    
+    for ik_, (k, ik) in enumerate(k_index_map.items()):
+        kx, ky = k
+        ikx, iky = ik
+        k_sq = kx**2 + ky**2
+        for im_, (m, im) in enumerate(k_index_map.items()):
+            mx, my = m
+            imx, imy = im
+            m_sq = mx**2 + my**2
+            psi1_m = psi1_hat[:, im_]
+            n = (kx-mx, ky-my)
+            if n in k_index_map:
+                in_ = list_dic_k_map.index(n)
+                psi1_n = psi1_hat[:, in_]
+                h_n = h_hat[in_]
+                det_mn = np.linalg.det(np.array([m, n]))
+                nonlinear_sum_A0[:, ik_] -= det_mn * ((k_sq + kd**2/2) * (m_sq + kd**2/2) * psi1_n*psi1_m)
+                nonlinear_sum_a0[:, ik_] -= det_mn * (kd**2/2 * (m_sq + kd**2/2) * psi1_n*psi1_m)
+                nonlinear_sum_A1[:, ik_, im_] += det_mn * kd**2/2 * (k_sq * psi1_n - h_n)
+                nonlinear_sum_a1[:, ik_, im_] -= det_mn * (k_sq * kd**2/2 * psi1_n + (k_sq + kd**2/2) * h_n) 
+
+    nonlinear_sum_A0 = nonlinear_sum_A0 / K**2
+    nonlinear_sum_a0 = nonlinear_sum_a0 / K**2
+    nonlinear_sum_A1 = nonlinear_sum_A1 / K**2
+    nonlinear_sum_a1 = nonlinear_sum_a1 / K**2
+    
+    return nonlinear_sum_A0, nonlinear_sum_a0, nonlinear_sum_A1, nonlinear_sum_a1
+
+
 def get_A_CG(K, KX, KY, k_index_map, kd, beta, kappa, nu, U, psi1_hat, h_hat):
     '''
     1. The mode truncation should be done outside this function. 
@@ -55,38 +100,9 @@ def get_A_CG(K, KX, KY, k_index_map, kd, beta, kappa, nu, U, psi1_hat, h_hat):
     linear_a1_diag = dX * (K_squared_kd2 * beta + K_squared2 * U) - K_squared_kd2 * kappa * K_squared - nu * K_squared4 * invCk
     linear_A1 = np.tile(np.diag(linear_A1_diag)[None,:,:], (N,1,1)) 
     linear_a1 = np.tile(np.diag(linear_a1_diag)[None,:,:], (N,1,1)) 
-    
-    # nonlinear summation part for A0, a0, A1 and a1
-    nonlinear_sum_A0 = np.zeros_like(psi1_hat, dtype=complex)
-    nonlinear_sum_a0 = np.zeros_like(psi1_hat, dtype=complex)
-    nonlinear_sum_A1 = np.zeros_like(linear_A1, dtype=complex)
-    nonlinear_sum_a1 = np.zeros_like(linear_a1, dtype=complex)
-    
-    for ik_, (k, ik) in enumerate(k_index_map.items()):
-        kx, ky = k
-        ikx, iky = ik
-        k_sq = kx**2 + ky**2
-        for im_, (m, im) in enumerate(k_index_map.items()):
-            mx, my = m
-            imx, imy = im
-            m_sq = mx**2 + my**2
-            psi1_m = psi1_hat[:, im_]
-            n = (kx-mx, ky-my)
-            if n in k_index_map:
-                in_ = list_dic_k_map.index(n)
-                psi1_n = psi1_hat[:, in_]
-                h_n = h_hat[in_]
-                det_mn = np.linalg.det(np.array([m, n]))
-                nonlinear_sum_A0[:, ik_] -= det_mn * ((k_sq + kd**2/2) * (m_sq + kd**2/2) * psi1_n*psi1_m)
-                nonlinear_sum_a0[:, ik_] -= det_mn * (kd**2/2 * (m_sq + kd**2/2) * psi1_n*psi1_m)
-                nonlinear_sum_A1[:, ik_, im_] += det_mn * kd**2/2 * (k_sq * psi1_n - h_n)
-                nonlinear_sum_a1[:, ik_, im_] -= det_mn * (k_sq * kd**2/2 * psi1_n + (k_sq + kd**2/2) * h_n) 
-        
-    nonlinear_sum_A0 = nonlinear_sum_A0 / K**2
-    nonlinear_sum_a0 = nonlinear_sum_a0 / K**2
-    nonlinear_sum_A1 = nonlinear_sum_A1 / K**2
-    nonlinear_sum_a1 = nonlinear_sum_a1 / K**2
-    
+
+    nonlinear_sum_A0, nonlinear_sum_a0, nonlinear_sum_A1, nonlinear_sum_a1 = get_A_CG_nonlinear(K, psi1_hat, h_hat, linear_A1, linear_a1, k_index_map, list_dic_k_map, kd)
+
     # aggregate 
     A0 = linear_A0 + nonlinear_sum_A0
     a0 = linear_a0 + nonlinear_sum_a0
@@ -163,6 +179,35 @@ def forward_OU(N, N_chunk, K, dt, x, y, r1, r2, mu0, a0, a1, R0, InvBoB, Sigma_u
             R_t[:, i] = np.diag(R)
             mu0 = mu
             R0 = R
+
+    return mu_t, R_t
+
+
+def forward_OU_flow(N, N_chunk, dt, psi1_k_t, mu0, R0, mu_t, R_t, A0_, A1, a0_, a1, F1, F2, Sigma1, Sigma2, sigma1, sigma2):
+    boB = np.diag(sigma1 * Sigma1.conj() + sigma2 * Sigma2.conj())
+    InvBoB = 1 / (Sigma1 * Sigma1.conj() + Sigma2 * Sigma2.conj())
+    InvBoB[0] = 0
+    bob = np.diag(sigma1 * sigma1.conj() + sigma2 * sigma2.conj())
+    R0_A1_H = R0 * A1.conj()
+    R0_a1_H = R0 * a1.conj()
+    for i in range(1, N):
+        i_chunk = (i-1) % N_chunk
+        if i_chunk == 0:
+            A0_t, a0_t = get_A_OU_flow(psi1_k_t[i-1:i-1+N_chunk, :], A0_, a0_, F1, F2)
+
+        # precompute
+        A0 = A0_t[i_chunk, :]
+        a0 = a0_t[i_chunk, :]
+        psi1_diff = psi1_k_t[i, :] - psi1_k_t[i-1, :]
+
+        # Update the posterior mean and posterior covariance
+        mu = mu0 + (a0 + a1 * mu0) * dt + (boB + R0_A1_H) * InvBoB @ (psi1_diff - (A0 + A1 * mu0) * dt)
+        R = R0 + (R0_a1_H.conj().T + R0_a1_H + bob - (boB + R0_A1_H) * InvBoB @ (boB + R0_A1_H).conj().T) * dt
+
+        mu_t[:, i] = mu
+        R_t[:, i] = np.diag(R)
+        mu0 = mu
+        R0 = R
 
     return mu_t, R_t
 
@@ -264,7 +309,7 @@ def back_sampling(N_s, dt, mu_t, R_t, Sigma, a0, a1):
 
 
 class Lagrangian_DA_OU:
-    def __init__(self, N, N_chunk, K, psi_k_t, tau_k_t, r1, r2, dt, sigma_xy, f, gamma, omega, sigma, xt, yt, r_cut, style='circle', corr_noise=False, **kargs):
+    def __init__(self, K, r1, r2, f, gamma, omega, sigma, r_cut, style='circle', corr_noise=False, **kargs):
         """
         Parameters:
         - N: int, total number of steps
@@ -286,33 +331,21 @@ class Lagrangian_DA_OU:
         - r_cut: modes truncation radius
         - corr_noise: LSMs with correlalted noise
         """
-        self.N = N
-        self.N_chunk = N_chunk
+
         self.K = K
-        self.dt = dt
         self.style = style
-        self.InvBoB = 1 / sigma_xy**2
-        self.mu0 = np.concatenate((truncate(psi_k_t[:,:,0],r_cut, style=style), truncate(tau_k_t[:,:,0],r_cut, style=style))) # assume the initial condition is truth
-        self.n = self.mu0.shape[0]
-        # self.R0 = np.eye(self.n, dtype='complex')
-        self.R0 = np.zeros((self.n,self.n), dtype='complex')
-        self.mu_t = np.zeros((self.n, N), dtype='complex')  # posterior mean
-        self.mu_t[:, 0] = self.mu0
-        self.R_t = np.zeros((self.n, N), dtype='complex')  # posterior covariance
-        self.R_t[:, 0] = np.diag(self.R0)  # only save the diagonal elements
-        self.a0 = truncate(f,r_cut, style=style).flatten(order='F')
-        self.a1 = -np.diag(truncate(gamma,r_cut, style=style).flatten(order='F')) + 1j * np.diag(truncate(omega,r_cut, style=style).flatten(order='F'))
-        self.Sigma_u = np.diag(truncate(sigma,r_cut, style=style).flatten(order='F'))
-        self.x = xt
-        self.y = yt
         self.r_cut = r_cut
         Kx = np.fft.fftfreq(K) * K
         Ky = np.fft.fftfreq(K) * K
         KX, KY = np.meshgrid(Kx, Ky)
         self.KX = truncate(KX, r_cut, style)
         self.KY = truncate(KY, r_cut, style)
-        self.r1 = truncate(r1[:,:,0], r_cut, style)
-        self.r2 = truncate(r2[:,:,0], r_cut, style)
+        self.r1 = truncate(r1, r_cut, style)
+        self.r2 = truncate(r2, r_cut, style)
+        self.f = truncate(f,r_cut, style=style)
+        self.gamma = truncate(gamma,r_cut, style=style)
+        self.omega = truncate(omega,r_cut, style=style)
+        self.sigma = truncate(sigma,r_cut, style=style)
         self.corr_noise = corr_noise
         
         if corr_noise:
@@ -322,11 +355,68 @@ class Lagrangian_DA_OU:
             self.Sigma_u[:d_cov, d_cov:] = np.diag(cov)
             self.Sigma_u[d_cov:, :d_cov] = np.diag(cov)  
             self.Sigma_u = np.nan_to_num(self.Sigma_u) # some modes not evaluated are nan
-            self.Sigma_u = self.Sigma_u * np.sqrt(2) # normailzation term due to real vs complex white noise
+            self.Sigma_u = self.Sigma_u * np.sqrt(2) # normalization term due to real vs complex white noise
 
 
-    def forward(self):
-        mu_t, R_t = forward_OU(self.N, self.N_chunk, self.K, self.dt, self.x, self.y, self.r1, self.r2, self.mu0, self.a0, self.a1, self.R0, self.InvBoB, self.Sigma_u, self.mu_t, self.R_t, self.KX, self.KY, self.corr_noise)
+    def forward(self, N, N_chunk, dt, tracer=True, **kargs):
+        r_cut = self.r_cut
+        style = self.style
+        r1 = self.r1
+        r2 = self.r2
+        gamma = self.gamma
+        omega = self.omega
+        f = self.f
+        sigma = self.sigma
+
+        if tracer == True:
+            psi_k_t = kargs['psi_k_t']
+            tau_k_t = kargs['tau_k_t']
+            sigma_xy = kargs['sigma_xy']
+            xt = kargs['xt']
+            yt = kargs['yt']
+
+            InvBoB = 1 / sigma_xy**2
+            mu0 = np.concatenate((truncate(psi_k_t[:,:,0],r_cut, style=style), truncate(tau_k_t[:,:,0],r_cut, style=style))) # assume the initial condition is truth
+            K_ = mu0.shape[0]
+            # self.R0 = np.eye(K_, dtype='complex')
+            R0 = np.zeros((K_, K_), dtype='complex')
+            mu_t = np.zeros((K_, N), dtype='complex')  # posterior mean
+            mu_t[:, 0] = mu0
+            R_t = np.zeros((K_, N), dtype='complex')  # posterior covariance
+            R_t[:, 0] = np.diag(R0)  # only save the diagonal elements
+            a0 = self.f.flatten(order='F')
+            a1 = -np.diag(gamma.flatten(order='F')) + 1j * np.diag(omega.flatten(order='F'))
+            Sigma_u = np.diag(self.sigma.flatten(order='F'))
+
+            mu_t, R_t = forward_OU(N, N_chunk, self.K, dt, xt, yt, r1[:, 0], r2[:, 0], mu0, a0, a1, R0, InvBoB, Sigma_u, mu_t, R_t, self.KX, self.KY, self.corr_noise)
+
+        elif tracer == False:
+            psi1_k_t = kargs['psi1_k_t']
+            psi2_k_t0 = kargs['psi2_k_t0']
+            psi1_k_t = truncate(psi1_k_t, r_cut, style)
+
+            mu0 = truncate(psi2_k_t0, r_cut, style) # assume the initial condition is truth
+            K_ = mu0.shape[0]
+            mu_t = np.zeros((K_, N), dtype='complex') # posterior mean
+            mu_t[:, 0] = mu0
+            R0 = np.zeros((K_, K_), dtype='complex')
+            R_t = np.zeros((K_, N), dtype='complex')  # posterior covariance
+            R_t[:, 0] = np.diag(R0) # only save the diagonal elements
+
+            deno = 1 / r1[:, 0] * r2[:, 1] - r1[:, 1] * r2[:, 0]
+            deno[0] = 0
+            A0_ = (r1[:, 0] * r2[:, 1] * (-gamma[:, 0] + 1j * omega[:, 0]) - r1[:, 1] * r2[:, 0] * (-gamma[:, 1] + 1j * omega[:, 1])) * deno
+            A1 = (r1[:, 0] * r2[:, 0] * (-(-gamma[:, 0] + gamma[:, 1]) + 1j * (-omega[:, 0] + omega[:, 1]))) * deno
+            a0_ = (r1[:, 1] * r2[:, 1] * (-(gamma[:, 0] - gamma[:, 1]) + 1j * (omega[:, 0] - omega[:, 1]))) * deno
+            a1 = (-r1[:, 1] * r2[:, 0] * (-gamma[:, 0] + 1j * omega[:, 0]) + r1[:, 0] * r2[:, 1] * (-gamma[:, 1] + 1j * omega[:, 1])) * deno
+            F1 = r1[:, 0] * f[:, 0] + r2[:, 0] * f[:, 1]
+            F2 = r1[:, 1] * f[:, 0] + r2[:, 1] * f[:, 1]
+            Sigma1 = r1[:, 0] * sigma[:, 0]
+            Sigma2 = r2[:, 0] * sigma[:, 1]
+            sigma1 = r1[:, 1] * sigma[:, 0]
+            sigma2 = r2[:, 1] * sigma[:, 1]
+
+            mu_t, R_t = forward_OU_flow(N, N_chunk, dt, psi1_k_t.T, mu0, R0, mu_t, R_t, A0_, A1, a0_, a1, F1, F2, Sigma2, Sigma2, sigma1, sigma2)
 
         return mu_t, R_t
 
@@ -349,7 +439,6 @@ class Lagrangian_DA_CG:
         KX, KY = np.meshgrid(Kx, Ky)
         self.KX_cut = truncate(KX, r_cut, style)
         self.KY_cut = truncate(KY, r_cut, style)
-        self.k_index_map_cut = {(KX[iy, ix], KY[iy, ix]): (ix, iy) for ix in range(K) for iy in range(K) if (KX[iy, ix]**2 + KY[iy, ix]**2) <=r_cut**2}
         self.h_k_cut = truncate(h_hat, r_cut, style)
         self.sigma_1 = sigma_1
         self.sigma_2 = sigma_2
@@ -360,6 +449,23 @@ class Lagrangian_DA_CG:
         self.U = U
         self.style = style
         self.r_cut = r_cut
+
+        # self.k_index_map_cut = {(KX[iy, ix], KY[iy, ix]): (ix, iy) for ix in range(K) for iy in range(K) if (KX[iy, ix]**2 + KY[iy, ix]**2) <=r_cut**2}
+
+        # Create an empty Numba-typed dictionary with appropriate key and value types
+        k_index_map_cut = Dict.empty(
+            key_type=types.Tuple([types.float64, types.float64]),  
+            value_type=types.Tuple([types.int64, types.int64])   
+        )
+        # Populate the Numba-typed dictionary using a loop
+        for ix in range(K):
+            for iy in range(K):
+                kx = KX[iy, ix]
+                ky = KY[iy, ix]
+                if (kx**2 + ky**2) <= r_cut**2:
+                    k_index_map_cut[(kx, ky)] = (ix, iy)
+        self.k_index_map_cut = k_index_map_cut
+
 
     def forward(self, N, N_chunk, dt, N_s=1, **kargs):
         """
@@ -421,4 +527,41 @@ class Lagrangian_DA_CG:
 
 
             return mu_t, R_t
+
+    def forward_model(self, N, dt, psi1_k_t0, psi2_k_t0):
+        K = self.K
+        r_cut = self.r_cut
+        style = self.style
+        KX_cut = self.KX_cut
+        KY_cut = self.KY_cut
+        k_index_map_cut = self.k_index_map_cut
+        kd = self.kd
+        beta = self.beta
+        kappa = self.kappa
+        nu = self.nu
+        U = self.U
+        h_k_cut = self.h_k_cut
+        psi1_k_t0 = truncate(psi1_k_t0, r_cut, style)
+        psi2_k_t0 = truncate(psi2_k_t0, r_cut, style)
+        K_ = psi1_k_t0.shape[0]
+
+        psi1_k_t = np.zeros((K_, N), dtype='complex')
+        psi2_k_t = np.zeros((K_, N), dtype='complex')
+        psi1_k_t[:, 0] = psi1_k_t0
+        psi2_k_t[:, 0] = psi2_k_t0
+
+        for i in range(1, N):
+            A0, a0, A1, a1= get_A_CG(K, KX_cut, KY_cut, k_index_map_cut, kd, beta, kappa, nu, U, psi1_k_t[:, i-1][None, :], h_k_cut)
             
+            A0 = np.squeeze(A0)
+            a0 = np.squeeze(a0)
+            A1 = np.squeeze(A1)
+            a1 = np.squeeze(a1)
+
+            # forward integration
+            psi1_k_t[:, i] = psi1_k_t[:, i-1] + (A0 + A1 @ psi2_k_t[:, i-1]) * dt
+            psi2_k_t[:, i] = psi2_k_t[:, i-1] + (a0 + a1 @ psi2_k_t[:, i-1]) * dt
+
+        return psi1_k_t, psi2_k_t
+
+                
