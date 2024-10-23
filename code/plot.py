79f4cefc36fb1cf1ca15b0d5cb2c8c21adea9f80
Zhongrui Wang <55739645+zhongruiw@users.noreply.github.com>
2024-10-23 01:48:48 -0500

    update plot.py

--- code/plot.py
+++ code/plot.py
@@ -23,6 +23,35 @@ def ifftnroll(K, q1_k_t, max_imag_lim=1e-8):
     return q1_t
 
 
+# @jit(nopython=True)
+def loop_ifft2_var(K, KX, KY, xgrid, ygrid, var_real, var_imag, var):
+    for jx in range(K):
+        for jy in range(K):
+            exp_cos = np.cos(KX[None,:] * xgrid[jx] + KY[None,:] * ygrid[jy])
+            exp_sin = np.sin(KX[None,:] * xgrid[jx] + KY[None,:] * ygrid[jy])
+            var_ = (exp_cos**2 @ var_real[:,None] + exp_sin**2 @ var_imag[:,None])[0,0] / K**4
+            var[jy, jx] = np.real(var_)
+    return var
+
+
+def ifft2_var(K, var_real, var_imag, r_cut, style='circle'):
+    '''
+    var = 1/K^4 * (\sum_k{var_real_k * cos^2(kx) + var_imag_k * sin^2(kx)})
+    '''
+    xgrid = np.linspace(-np.pi,np.pi, K, endpoint=False)
+    ygrid = np.linspace(-np.pi,np.pi, K, endpoint=False)
+    Kx = np.fft.fftfreq(K) * K
+    Ky = np.fft.fftfreq(K) * K
+    KX, KY = np.meshgrid(Kx, Ky)
+    KX = truncate(KX, r_cut, style)
+    KY = truncate(KY, r_cut, style)
+    var = np.zeros((K, K))
+
+    var = loop_ifft2_var(K, KX, KY, xgrid, ygrid, var_real, var_imag, var)
+    
+    return var
+
+
 def psi2q(psi1_k, psi2_k, kd, h_k):
     K = psi1_k.shape[0]
     
@@ -44,27 +73,35 @@ def psi2q(psi1_k, psi2_k, kd, h_k):
     return q1_k, q2_k
 
 
-def plot_contour_fields(q1, q2, title):
-    fig, axes = plt.subplots(1, 2, figsize=(7, 3))
-    
-    K = q1.shape[0]
-    x = np.linspace(-np.pi, np.pi, K)
-    y = np.linspace(-np.pi, np.pi, K)
-    X, Y = np.meshgrid(x, y)
-    
-    contour1 = axes[0].contourf(X, Y, q1, levels=128, cmap='seismic')
-    axes[0].set_title('upper layer PV: '+title)
-    fig.colorbar(contour1, ax=axes[0])
-    
-    contour2 = axes[1].contourf(X, Y, q2, levels=128, cmap='seismic')
-    axes[1].set_title('lower layer PV: '+title)
-    fig.colorbar(contour2, ax=axes[1])
-    
-    plt.tight_layout()
+def plot_contour_fields(q1, q2, title, colorlim=None):
+	fig, axes = plt.subplots(1, 2, figsize=(7, 3))
 
+	K = q1.shape[0]
+	x = np.linspace(-np.pi, np.pi, K)
+	y = np.linspace(-np.pi, np.pi, K)
+	X, Y = np.meshgrid(x, y)
 
-def plot_psi_k_seriespdf(dt, sel0, sel1, ikx, iky, interv, xlim, ylim, xt, yt, psi1_k, psi2_k, labels):
-	colors = ['k', 'r', 'c', 'b']
+	if colorlim is None:
+		vmax1 = np.max(abs(q1))
+		vmax2 = np.max(abs(q2))
+	else:
+		vmax1, vmax2 = colorlim
+
+	levels1 = np.linspace(-vmax1, vmax1, 257)
+	levels2 = np.linspace(-vmax2, vmax2, 257)
+
+	contour1 = axes[0].contourf(X, Y, q1, levels=levels1, cmap='seismic')
+	axes[0].set_title('upper layer PV: '+title)
+	fig.colorbar(contour1, ax=axes[0])
+
+	contour2 = axes[1].contourf(X, Y, q2, levels=levels2, cmap='seismic')
+	axes[1].set_title('lower layer PV: '+title)
+	fig.colorbar(contour2, ax=axes[1])
+
+	plt.tight_layout()
+	
+
+def plot_psi_k_seriespdf(dt, sel0, sel1, ikx, iky, interv, xlim, ylim, xt, yt, psi1_k, psi2_k, labels, colors):
 	xaxis = np.arange(sel0*dt, sel1*dt, interv*dt)
 	fig = plt.figure(figsize=(16,5))
 	widths = [5, 1, 5, 1]
@@ -198,8 +235,141 @@ def plot_psi_k_seriespdf(dt, sel0, sel1, ikx, iky, interv, xlim, ylim, xt, yt, p
 	plt.tight_layout()
 
 
-def plot_layer_seriespdf(dt, sel0, sel1, ikx, iky, interv, xlim, ylim, psi1, psi2, labels):
-	colors = ['k', 'r', 'c', 'b']
+def plot_psi1_k_seriespdf(dt, sel0, sel1, ikx, iky, interv, xlim, ylim, xt, yt, psi1_k, psi2_k, labels, colors):
+    xaxis = np.arange(sel0*dt, sel1*dt, interv*dt)
+    fig = plt.figure(figsize=(16,5))
+    widths = [5, 1, 5, 1]
+    heights = [2, 2, 2]
+    spec = fig.add_gridspec(ncols=4, nrows=3, width_ratios=widths, height_ratios=heights)
+    
+    plt.subplots_adjust(wspace=0.35, hspace=0.5)     # Adjust the overall spacing of the figure
+    ax1 = fig.add_subplot(spec[0, 0])
+    ax2 = fig.add_subplot(spec[1, 0])
+    ax3 = fig.add_subplot(spec[2, 0])
+    ax4 = fig.add_subplot(spec[0, 1])
+    ax5 = fig.add_subplot(spec[1, 1])
+    ax6 = fig.add_subplot(spec[2, 1])
+    ax11 = fig.add_subplot(spec[0, 2])
+    ax22 = fig.add_subplot(spec[1, 2])
+    ax33 = fig.add_subplot(spec[2, 2])
+    ax44 = fig.add_subplot(spec[0, 3])
+    ax55 = fig.add_subplot(spec[1, 3])
+    ax66 = fig.add_subplot(spec[2, 3])
+    
+    # plot time series
+    ax1.plot(xaxis, xt[0,sel0:sel1:interv], 'k',label='x')
+    ax1.set_xlim(sel0*dt, sel1*dt)
+    ax1.set_ylabel('x')
+    ax1.set_title('time series')
+    ax1.set_xlim(xlim)
+    
+    ax11.plot(xaxis, yt[0,sel0:sel1:interv], 'k')
+    ax11.set_xlim(sel0*dt, sel1*dt)
+    ax11.set_ylabel('y')
+    ax11.set_title('time series')
+    ax11.set_xlim(xlim)
+    
+    # plot pdf
+    samples = xt[0, :]
+    kde = gaussian_kde(samples)
+    xticks = np.linspace(samples.min(), samples.max(), 100)
+    p = kde.evaluate(xticks)
+    ax4.plot(xticks, p, 'k')
+    mean, std = samples.mean(), samples.std() # Fit a Gaussian to the same data
+    gaussian_pdf = norm.pdf(xticks, mean, std)  # Calculate the Gaussian PDF
+    ax4.plot(xticks, gaussian_pdf, 'k--', label='Fitted Gaussian')  # Dashed line for Gaussian
+    ax4.set_title('PDF')
+    ax4.set_yscale('log', base=10) 
+    
+    # plot pdf
+    samples = yt[0, sel0:sel1]
+    kde = gaussian_kde(samples)
+    xticks = np.linspace(samples.min(), samples.max(), 100)
+    p = kde.evaluate(xticks)
+    ax44.plot(xticks, p, 'k')
+    mean, std = samples.mean(), samples.std() # Fit a Gaussian to the same data
+    gaussian_pdf = norm.pdf(xticks, mean, std)  # Calculate the Gaussian PDF
+    ax44.plot(xticks, gaussian_pdf, 'k--', label='Fitted Gaussian')  # Dashed line for Gaussian
+    ax44.set_title('PDF')
+    ax44.set_yscale('log', base=10) 
+    
+    for i, data in enumerate(psi1_k):
+        ax2.plot(xaxis, data[iky,ikx,sel0:sel1:interv].real, colors[i], label=labels[i])
+        ax22.plot(xaxis, data[iky,ikx,sel0:sel1:interv].imag, colors[i], label=labels[i])
+    
+        samples = data[iky, ikx, sel0:sel1].real
+        kde = gaussian_kde(samples)
+        xticks = np.linspace(samples.min(), samples.max(), 100)
+        p = kde.evaluate(xticks)
+        ax5.plot(xticks, p, colors[i], label=labels[i])
+    
+        samples = data[iky, ikx, sel0:sel1].imag
+        kde = gaussian_kde(samples)
+        xticks = np.linspace(samples.min(), samples.max(), 100)
+        p = kde.evaluate(xticks)
+        ax55.plot(xticks, p, colors[i], label=labels[i])
+    
+    ax2.set_ylabel(r'$Re(\hat{{\psi}}_{{1,({:d},{:d})}})$'.format(ikx, iky))
+    ax2.set_xlim(xlim)
+    ax22.set_ylabel(r'$Im(\hat{{\psi}}_{{1,({:d},{:d})}})$'.format(ikx, iky))
+    ax22.set_xlim(xlim)
+    
+    samples = psi1_k[0][iky, ikx, sel0:sel1].real
+    mean, std = samples.mean(), samples.std() # Fit a Gaussian to the same data
+    gaussian_pdf = norm.pdf(xticks, mean, std)  # Calculate the Gaussian PDF
+    ax5.plot(xticks, gaussian_pdf, 'k--', label='Fitted Gaussian')  # Dashed line for Gaussian
+    ax5.set_yscale('log', base=10) 
+    ax5.set_ylim(ylim[0], np.max(p)+ylim[1])
+    
+    samples = psi1_k[0][iky, ikx, sel0:sel1].imag
+    mean, std = samples.mean(), samples.std() # Fit a Gaussian to the same data
+    gaussian_pdf = norm.pdf(xticks, mean, std)  # Calculate the Gaussian PDF
+    ax55.plot(xticks, gaussian_pdf, 'k--', label='Fitted Gaussian')  # Dashed line for Gaussian
+    ax55.set_yscale('log', base=10) 
+    ax55.set_ylim(ylim[0], np.max(p)+ylim[1])
+
+    for i, data in enumerate(psi2_k):
+        ax3.plot(xaxis, data[iky,ikx,sel0:sel1:interv].real, colors[i], label=labels[i])
+        ax33.plot(xaxis, data[iky,ikx,sel0:sel1:interv].imag, colors[i], label=labels[i])
+    
+        samples = data[iky, ikx, sel0:sel1].real
+        kde = gaussian_kde(samples)
+        xticks = np.linspace(samples.min(), samples.max(), 100)
+        p = kde.evaluate(xticks)
+        ax6.plot(xticks, p, colors[i], label=labels[i])
+    
+        samples = data[iky, ikx, sel0:sel1].imag
+        kde = gaussian_kde(samples)
+        xticks = np.linspace(samples.min(), samples.max(), 100)
+        p = kde.evaluate(xticks)
+        ax66.plot(xticks, p, colors[i], label=labels[i])
+
+    ax3.set_ylabel(r'$Re(\hat{{\psi}}_{{2,({:d},{:d})}})$'.format(ikx, iky))
+    ax3.set_xlabel('t')
+    ax3.set_xlim(xlim)
+    ax3.legend(prop={'size': 8})
+    ax33.set_ylabel(r'$Im(\hat{{\psi}}_{{2,({:d},{:d})}})$'.format(ikx, iky))
+    ax33.set_xlabel('t')
+    ax33.set_xlim(xlim)
+    ax33.legend(prop={'size': 8})
+    
+    samples = psi2_k[0][iky, ikx, sel0:sel1].real
+    mean, std = samples.mean(), samples.std() # Fit a Gaussian to the same data
+    gaussian_pdf = norm.pdf(xticks, mean, std)  # Calculate the Gaussian PDF
+    ax6.plot(xticks, gaussian_pdf, 'k--', label='Fitted Gaussian')  # Dashed line for Gaussian
+    ax6.set_ylim(ylim[0], np.max(p)+ylim[1])
+    ax6.set_yscale('log', base=10) 
+    
+    samples = psi2_k[0][iky, ikx, sel0:sel1].imag
+    mean, std = samples.mean(), samples.std() # Fit a Gaussian to the same data
+    gaussian_pdf = norm.pdf(xticks, mean, std)  # Calculate the Gaussian PDF
+    ax66.plot(xticks, gaussian_pdf, 'k--', label='Fitted Gaussian')  # Dashed line for Gaussian
+    ax66.set_ylim(ylim[0], np.max(p)+ylim[1])
+    ax66.set_yscale('log', base=10) 
+    plt.tight_layout()
+
+
+def plot_layer_seriespdf(dt, sel0, sel1, ikx, iky, interv, xlim, ylim, psi1, psi2, labels, colors):
 	xaxis = np.arange(sel0*dt, sel1*dt, interv*dt)
 
 	fig = plt.figure(figsize=(10,4))
@@ -254,4 +424,35 @@ def plot_layer_seriespdf(dt, sel0, sel1, ikx, iky, interv, xlim, ylim, psi1, psi
 	ax4.set_yscale('log', base=10) 
 	ax4.set_title('PDF')
 	ax4.set_ylim(ylim[0], np.max(p)+ylim[1])
+
+
+def plot_rmses(dt, sel0, sel1, s_rate, interv, xlim, data1, data2, labels, colors):
+	xaxis = np.arange(sel0*(s_rate)*dt, sel1*(s_rate)*dt, interv*s_rate*dt)
+
+	fig = plt.figure(figsize=(8,4))
+	widths = [7]
+	heights = [1, 1]
+	spec = fig.add_gridspec(ncols=1, nrows=2, width_ratios=widths, height_ratios=heights)
+
+	plt.subplots_adjust(wspace=0.35, hspace=0.5)     # Adjust the overall spacing of the figure
+	ax1 = fig.add_subplot(spec[0, 0])
+	ax2 = fig.add_subplot(spec[1, 0])
+
+	# plot time series
+	for i, data in enumerate(data1):
+		ax1.plot(xaxis, data[sel0:sel1:interv], colors[i], label=labels[i])
+
+	ax1.set_xlim(sel0*(s_rate)*dt, sel1*(s_rate)*dt)
+	ax1.set_ylabel(r'$RMSE\ \psi_1$')
+	ax1.set_xlim(xlim)
+
+	# plot time series
+	for i, data in enumerate(data2):
+		ax2.plot(xaxis, data[sel0:sel1:interv], colors[i], label=labels[i])
+
+	ax2.set_xlim(sel0*(s_rate)*dt, sel1*(s_rate)*dt)
+	ax2.set_ylabel(r'$RMSE\ \psi_2$')
+	ax2.set_xlabel('t')
+	ax2.set_xlim(xlim)
+	ax2.legend(prop={'size': 8})
 
